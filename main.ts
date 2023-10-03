7876c191a3e845caeb0b7ec2fdd855bd56a1eb82
kyle-lyver <144264052+kyle-lyver@users.noreply.github.com>
2023-10-03 13:26:15 -0400

    spaces

--- main.ts
+++ main.ts
@@ -8,18 +8,18 @@
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
-// variable 
+// variable
 let cookie: number
 cookie = 0
 
 // input
-input.onButtonPressed(Button.A, function() {
+input.onButtonPressed (Button.A, function() {
   cookie = (cookie + 1)
   basic.showNumber(cookie)
 })
 
 // output
-input.onButtonPressed(Button.B, function() {
+input.onButtonPressed (Button.B, function() {
   cookie = 0
   basic.showNumber(cookie)
 })
